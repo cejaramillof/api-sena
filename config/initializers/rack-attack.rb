81f8e49dac0bc0182ff29c9f9db609d6c29cfb87
Carlos Jaramillo <cejaramillof@gmail.com>
2017-05-20 12:05:01 -0500

    fix rack

--- config/initializers/rack-attack.rb
+++ config/initializers/rack-attack.rb
@@ -6,7 +6,7 @@ class Rack::Attack
     '127.0.0.1' == req.ip || '::1' == req.ip
   end
   # Allow an IP address to make 5 requests every 5 seconds
-  throttle('req/ip', limit: 5, period: 60) do |req|
+  throttle('req/ip', limit: 5, period: 5) do |req|
     req.ip
   end
   # Send the following response to throttled clients
